1b3772e1bcc1d826a187525085ff9bb8d18deb1b
FazioNico <contact@nicolasfazio.ch>
2019-05-28 11:51:00 +0200

    add firebase

--- src/app/app.js
+++ src/app/app.js
@@ -1,14 +1,49 @@
 import { LoginPage } from './features/login/login-page';
+import { DashboardPage } from './features/dashboard/dashboard-page';
 import '../theme/colors.css';
 import '../theme/styles.css';
+
 export class MyApp{
   constructor({selector = null}) {
     this.root = selector;
     console.log('hello');
-    
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyCs35yK8j8Bvr4wjNL1zaOs5ohO4p-LTOw",
+      authDomain: "fir-fb-ba1f3.firebaseapp.com",
+      databaseURL: "https://fir-fb-ba1f3.firebaseio.com",
+      projectId: "fir-fb-ba1f3",
+      storageBucket: "fir-fb-ba1f3.appspot.com",
+      messagingSenderId: "989834761689",
+      appId: "1:989834761689:web:69e12fde7f619e13"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    // utilisation de fb realtime db
+    this.database = firebase.database();
+    this.auth = firebase.auth();
+    this.googleProvider = new firebase.auth.GoogleAuthProvider();
+
   }
   start() {
-    new LoginPage({selector: this.root})
+
+    this.auth.onAuthStateChanged((user) => {
+      if (user) {
+        new DashboardPage({
+          selector: this.root,
+          email: user.email
+        })
+      }
+      else {
+        new LoginPage({
+          selector: this.root,
+          auth: this.auth,
+          googleProvider: this.googleProvider
+        });
+      }
+    });
+    
   }
 }
 new MyApp({selector: 'app'}).start();
--- src/app/features/login/login-page.js
+++ src/app/features/login/login-page.js
@@ -1,34 +1,46 @@
 import { DashboardPage } from '../dashboard/dashboard-page';
 
 export class LoginPage {
-  constructor({selector = null}) {
+  constructor({selector = null, auth = null, googleProvider = null}) {
     this.root = selector;
+    this.auth = auth;
+    this.googleProvider = googleProvider;
     this.loadUI();
-    this.loadEventUI();
+    this.loadEventUI()
   }
 
   loadUI() {
     document.querySelector(this.root).innerHTML = `
       <h1>Welcome</h1>
-      <form>
-        <input type="email" placeholder="email" value="fazio@aa.ch"><br/>
-        <input type="password" placeholder="password"><br/>
-        <button>login</button>
-      </form>
+      <button>login / signin</button>
     `;
   }
 
   loadEventUI() {
-    document.querySelector(this.root + ' form').addEventListener('submit', e => this.login(e))
+    document.querySelector(this.root + ' button').addEventListener('click', _ => this.login())
   }
 
-  login(e) {
-    e.preventDefault();
-    const email = e.target.querySelector('input[type=email]').value;
-    console.log(email);
-    new DashboardPage({
-      selector: this.root,
-      email
-    })
+  login() {
+    this.auth.signInWithPopup(this.googleProvider)
+        .then((result)=> {
+          // This gives you a Google Access Token.
+          // You can use it to access the Google API.
+          let token = result.credential.accessToken;
+          // The signed-in user info.
+          let user = result.user;
+          console.log('--->', user);
+          // ...
+        }).catch((error)=> {
+          console.log('--->', error);
+          // Handle Errors here.
+          let errorCode = error.code;
+          let errorMessage = error.message;
+          // The email of the user's account used.
+          let email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          let credential = error.credential;
+          alert(errorMessage);
+          // ...
+        });
   }
 }
